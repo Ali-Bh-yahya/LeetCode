2a38897abe0a937ccfa5f4b1c8d48dac0335f0ea
Ali <Ali.BahaYahya@gmail.com>
2026-08-21 15:43:15 +0300

    implement the addLast ,  addFirst ,  addbyIndex , isEmpty and Size  m,ethod

--- LinkedLists/linkedList/src/main/java/com/mycompany/linkedlist/LinkedList.java
+++ LinkedLists/linkedList/src/main/java/com/mycompany/linkedlist/LinkedList.java
@@ -1,16 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+//A Linked List is a linear data structure where elements (called nodes) are linked using pointers.
+//node contains two parts:
+//1-Data: Stores the actual value.
+//2-Next: A reference (or pointer) to the next node in the list.
 
 package com.mycompany.linkedlist;
 
-/**
- *
- * @author DVDSTORE
- */
 public class LinkedList {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
+        //check the size :
+        int size = linkedList.size();
+        System.out.println(size);
+        //check if the linked list is Empty:
+        System.out.println(linkedList.isEmpty());
+        //Add the items to my linkedList:
+        linkedList.addLast(10);
+        System.out.println(linkedList);// the out put is  "com.mycompany.linkedlist.MyLinkedList@7344699f" it's a address 
+        // but when i implement the toString method it given me the value of the LinkedList
+        linkedList.addLast(120);
+        System.out.println(linkedList);
+        //Add the item to the first of the linkedList :
+        linkedList.addFirst(101);
+        System.out.println(linkedList);
     }
 }
--- LinkedLists/linkedList/src/main/java/com/mycompany/linkedlist/MyLinkedList.java
+++ LinkedLists/linkedList/src/main/java/com/mycompany/linkedlist/MyLinkedList.java
@@ -12,11 +12,81 @@ public class MyLinkedList<T> {
      private Node<T> head;
     private Node<T> tail;
     private int size;
-    
+    // Constructor
     public MyLinkedList(){
         head = null; 
         tail = null;
         size = 0;
+    }
+    
+    // size method :
+    public int size(){
+        return size;
+    }
+    
+    //isEmpty method :
+    public boolean isEmpty(){
+        return size == 0 ;
+    }
+    
+    //add to the last method :
+    public void addLast(T val){
+        Node<T> newNode = new Node<>(val);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+            
+        }
+        else{
+            tail.next = newNode;
+            tail = newNode;
+            
+        }
+        size ++;
+    }
+    
+    //toString method :
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        Node<T> currunt = head;
+        while(currunt != null){
+            str.append(currunt.data);
+            if(currunt.next != null)
+                str.append(" , ");
+            currunt = currunt.next;
+        }
+        return str.toString();
+    }
+    
+    //add to the first  method:
+    public void addFirst(T val){
+        Node<T> newNode = new Node<>(val);
+        newNode.next = head;
+        head =  newNode;
+        if(size == 0 )
+            tail = newNode;
+        size++;
+    }
+    
+    // add by the index method :
+    public void add(int index , T val){ 
+        if(index < 0 || index > size )
+            throw new ArrayIndexOutOfBoundsException("Invalid index: " + index + ", Size: " + size);
+        if(index == 0 )
+            addFirst(val);
+        else if (index == size)
+            addLast(val);
+        else{
+            Node<T> node = new Node<>(val);
+            Node<T> currunt = head;
+            
+            for( int i = 0 ; i < index-1 ; i++)
+                currunt = currunt.next; // 1 c=0- c-> 1 2- c=1 c-> 2 
+            node.next =currunt.next;
+            currunt.next = node;
+            size++;
+        }
+            
     }
     
 }
